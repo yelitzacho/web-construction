e15e12fb3df8ddfa031304c568d6d8e936a306cc
Yelitza Cho <yelitza.choque@gmail.com>
2018-01-24 10:20:42 -0500

    web-construction

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(window).load(function(){
+
+    $(function() {
+        $('#file-input').change(function(e) {
+            addImage(e);
+        });
+
+        function addImage(e){
+            var file = e.target.files[0],
+                imageType = /image.*/;
+
+            if (!file.type.match(imageType))
+                return;
+
+            var reader = new FileReader();
+            reader.onload = fileOnload;
+            reader.readAsDataURL(file);
+        }
+
+        function fileOnload(e) {
+            var result=e.target.result;
+            $('#imgSalida').attr("src",result);
+        }
+    });
+});
